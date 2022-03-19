606f3f6ed5d555ba996f2bff390b34f186cc4d69
Seew0 <96205648+Seew0@users.noreply.github.com>
2022-03-20 03:25:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ mkfs.ext4 /dev/sda1
 mount /dev/sda2 /mnt
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
-reflector --latest 50 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base linux linux-firmware base-devel vim
 genfstab -U /mnt >> /mnt/etc/fstab
-curl -LO raw.githubusercontent.com/Seew0/host/main/user-setup
+curl -LO raw.githubusercontent.com/Seew0/host/main/user-setup.sh
 cp user-setup /mnt/root
 echo "Copied file in root"
 arch-chroot /mnt sh /root/user-setup
