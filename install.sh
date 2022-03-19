059dab57f10aa0dec7eda1756b6cbdab1da08c29
Seew0 <96205648+Seew0@users.noreply.github.com>
2022-03-20 04:17:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cfdisk /dev/sda
+fdisk /dev/sda
+reflector --latest 50 --sort rate --save /etc/pacman.d/mirrorlist
 mkfs.ext4 /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 mount /dev/sda2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
-reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware base-devel vim
+pacstrap /mnt base linux base-devel vim linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-curl -LO raw.githubusercontent.com/Seew0/minimal-arch-linux/main/user-setup.sh
-cp user-setup.sh /mnt/root
+curl -LO raw.githubusercontent.com/Seew0/minimal-arch-linux/main/user
+cp two /mnt/root
 echo "Copied file in root"
-arch-chroot /mnt sh /root/user-setup.sh
-rm /mnt/root/user-setup.sh
+arch-chroot /mnt sh /root/user
+rm /mnt/root/user
 umount -a
 reboot
